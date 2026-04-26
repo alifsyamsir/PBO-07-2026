7fc0741830c8c4fff1cb78bb15a603a0fbf59a68
RandyRahmat <randyrahmat34@gmail.com>
2026-04-27 03:30:21 +0800

    Kumpul Tugas 2: Class, Object, Attribute, Behavior & Constructor

---
+++ H071251090/TuPrak2/Main.java
@@ -0,0 +1,15 @@
+package TuPrak2;
+
+public class Main {
+    public static void main(String[] args) {
+        Energi api = new Energi("Api", 25);
+        Energi listrik = new Energi("listrik", 20);
+        KartuPokemon charizard = new KartuPokemon("Charizard", 100, api);
+        KartuPokemon pikachu = new KartuPokemon("Pikachu", 80, listrik);
+
+        charizard.TampilStatus();
+        pikachu.TampilStatus();
+
+        charizard.Tanding(pikachu);
+    }
+}
